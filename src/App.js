835e5a73edf97a37f90a7de67c52fe71b42f1549
ruthmerygeraldine <ruthmeryghor@gmail.com>
2017-10-29 22:40:04 -0500

    analizando

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import truck from './image/track.svg'
+import { connect } from 'redux-zero/react'
+
 import logo from './logo.svg';
 import './App.css';
 
-const App = ({deedive}) => {
+const App = ({deepdive,contar, complet,comparation,marcar,respuestas}) => {
+  const preguntaActual = deepdive[contar];
   return (
     <div className="container">
       <header className="text-center">
@@ -17,7 +21,7 @@ const App = ({deedive}) => {
           </div>
        
         <div id="quiz">
-          <img src="image/plane.svg"/>
+          <img src={truck}/>
           <p></p>
         </div>
       </div>
@@ -28,4 +32,5 @@ const App = ({deedive}) => {
     </div>);
 }
 
-export default App;
+const maptoProps = ({deepdive,contar, complet,comparation,marcar,respuestas})=> ({deepdive,contar, complet,comparation,marcar,respuestas}); 
+export default connect(maptoProps) (App);
---
+++ src/store.js
@@ -0,0 +1,58 @@
+import createStore from 'redux-zero'
+import avion from './image/plane.svg';
+import barco from './image/bote.svg';
+import bicicleta from './image/bici.svg';
+import bus from './image/bus.svg';
+import carro from './image/car.svg';
+
+const deepdive = [
+    {
+      answer: 'Which is the oldest airline in the world?',
+      opciones: { A: 'Avianca', B: 'KLM', C: 'Qantas' },
+      result: 'KLM',
+      image: 'assets/image/plane.svg'
+    },
+    {
+      answer: 'Which is the largest port in the world?',
+      opciones: { A: 'Port of Shanghai', B: 'Port of Singapore', C: 'Port of Rotterdam' },
+      result: 'Port of Shanghai',
+      image: 'assets/image/bote.svg'
+    },
+    {
+      answer: 'What is the longest distance cycling backwards?',
+      opciones: { A: '89.30 km', B: '675.10 km', C: '337.60 km' },
+      result: '337.60 km',
+      image: 'assets/image/bici.svg'
+    },
+    {
+      answer: 'What is the highest speed ever reached by a school bus?',
+      opciones: { A: '590 km/h', B: '320 km/h', C: '245 km/h' },
+      result: '590 km/h',
+      image: 'assets/image/bus.svg'
+    },
+    {
+      answer: 'What is the longest car trip on one tank of gas?',
+      opciones: { A: '2617 km', B: '3568 km', C: '1732 km' },
+      result: '2617 km',
+      image: 'assets/image/car.svg'
+    }
+  ]
+
+const initialState = {
+    deepdive: deepdive,
+    contar: 0,
+    complet: false,
+    comparation: false,
+    marcar: true,
+    respuestas: []
+};
+
+const store = createStore(initialState);
+
+export default store;
+
+
+
+
+
+
